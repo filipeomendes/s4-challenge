881527b7f39bc240b4351c1375f013fa895d8aa8
MironMarins <mironcbio@gmail.com>
2023-11-11 16:12:32 -0300

    Adicionando classes na pagina principal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <div>
-        <Image src="mapa" alt="imagem ilustrativa mapa" width={320} height={320}></Image>
-        <div className="botao">
-          <Link to="/maps/solicitacao">
-            <button type="submit" className="botao-seguinte">Seguinte</button>
-          </Link>
+      <main className="main-mapa">
+        <div className="img-mapa">
+          <Image src="mapa" alt="imagem ilustrativa mapa" width={320} height={320}></Image>
+          <div className="botao">
+            <Link to="/maps/solicitacao">
+              <button type="submit" className="botao-seguinte">Seguinte</button>
+            </Link>
+          </div>
         </div>
-      </div>
+      </main>
     </>
   )
 }
