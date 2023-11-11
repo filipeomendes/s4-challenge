22d56888d8b55e648f14b6902d5ad25752c7212a
MironMarins <mironcbio@gmail.com>
2023-11-11 16:13:25 -0300

    Corrigindo nome da pagina principal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import { Link } from "react-router-dom";
 
-export default function Home() {
+export default function Maps() {
   return (
     <>
       <main className="main-mapa">
         <div className="img-mapa">
-          <Image src="mapa" alt="imagem ilustrativa mapa" width={320} height={320}></Image>
+          <Image src="/mapa" alt="imagem ilustrativa mapa" width={320} height={320}></Image>
           <div className="botao">
             <Link to="/maps/solicitacao">
               <button type="submit" className="botao-seguinte">Seguinte</button>
