03d45a31c5b04e40959bd3e8b29fd657225900d0
MironMarins <mironcbio@gmail.com>
2023-11-11 16:10:50 -0300

    montando estrutura da pagina principal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,17 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <>
-    
+      <div>
+        <Image src="mapa" alt="imagem ilustrativa mapa" width={320} height={320}></Image>
+        <div className="botao">
+          <Link to="/maps/solicitacao">
+            <button type="submit" className="botao-seguinte">Seguinte</button>
+          </Link>
+        </div>
+      </div>
     </>
   )
 }
