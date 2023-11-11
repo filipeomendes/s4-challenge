9ce98e443baf479326e69f591b1ee10967e30b8d
vinips1993 <RM550907@fiap.com.br>
2023-11-11 15:03:42 -0300

    Adicionando classes aos elementos do Rodape

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -4,16 +4,16 @@ export default function Rodape(){
 
     return(
         <footer>
-            <section>
-                <div>
-                    <nav>
+            <section className="conteudo-rodape">
+                <div className="redes-sociais-logos">
+                    <nav className="redes-sociais-nav">
                         <a href="https://github.com/filipeomendes/sprint3-challenge-1tdspv" target='blank'><Image src="logoGithub" alt="Logo preto do Github"></Image></a>
                         <a href="https://www.instagram.com/complex.wrld/" target='blank'><Image src="/logoInstagram" alt="Logo preto do Instagram"></Image></a>
                         <a href="https://www.facebook.com" target='blank'><Image src=""  alt="Logo preto do Facebook"></Image></a>
                     </nav>
                 </div>
-                <div>
-                    <p>Todos os direitos reservados ©2023 - Complex</p>
+                <div className='copyright'>
+                    <p className='texto-footer'>Todos os direitos reservados ©2023 - Complex</p>
                 </div>
             </section>
         </footer>
